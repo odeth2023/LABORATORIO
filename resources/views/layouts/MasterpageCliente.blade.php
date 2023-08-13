f0c96dd940c2ae337081999db8e75331454eaa3b
unknown <odeth18@hotmail.es>
2023-08-13 03:11:19 -0500

    v16

--- resources/views/layouts/MasterpageCliente.blade.php
+++ resources/views/layouts/MasterpageCliente.blade.php
@@ -166,7 +166,7 @@
     <script src="../assetsCustomer/js/Pelicula/mixitup.min.js"></script>
     <script src="../assetsCustomer/js/Pelicula/owl.carousel.min.js"></script>
     <script src="../assetsCustomer/js/Pelicula/main.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 
 </body>
